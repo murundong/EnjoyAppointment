a5f204d9c0951a641821eafeb7d018a428aaae7c
Roden <448544937@qq.com>
2020-11-09 01:39:40 +0800

    MainPage

---
+++ MockData/data.js
@@ -0,0 +1,56 @@
+
+var local_doors={
+  errCode:0,
+  total:3,
+  data:[
+    {
+      id:1,
+      img:'http://www.bundfm.com/uploadfile/2019/0610/20190610035519966.jpg',
+      status:'营业中',
+      name:'梅斯奔驰中心',
+      address:'上海市浦东新区世博大道1200号'
+    },
+    {
+      id:2,
+      img:'http://img.zjolcdn.com/pic/0/07/67/93/7679300_941970.jpg',
+      status:'预售中',
+      name:'国家体育馆',
+      address:'北京市朝阳区安定路甲3号'
+    },
+    {
+      id:3,
+      img:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2465275575,386684080&fm=26&gp=0.jpg',
+      status:'停业中',
+      name:'台北小巨蛋体育馆',
+      address:'中国台湾省台北市南京东路和敦化北路'
+    }
+    ]
+}
+
+var local_banners=[
+{
+  id:1,
+  img:'http://n.sinaimg.cn/sinacn10/480/w1280h800/20180613/f9e9-hcwpcmp9678486.jpg',
+  url:'123'
+
+},{
+  id:2,
+  img:'http://img.improve-yourmemory.com/pic/4f138809da94a79d0bf71b8881a84add-2.jpg',
+  url:'456'
+},{
+  id:3,
+  img:'http://www.aihami.com/uploads/allimg/180818/170-1PQP0525J41.jpg',
+  url:'789'
+}
+
+
+]
+
+
+// module.exports={
+//   local_banner:local_banner
+// }
+
+export{
+  local_banners,local_doors
+}
--- pages/main/main.js
+++ pages/main/main.js
@@ -1,39 +1,39 @@
 // pages/main/main.js
+
+// var local_banner = require('../../MockData/data.js')
+import {local_banners,local_doors} from '../../MockData/data.js'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-     Banners:['banner1','banner2','banner3'],
-     Doors:[{
-       img:'img1',
-       txt:'door1',
-       desc:'desc1',
-       status:'open'
-     },
-     {
-      img:'img2',
-      txt:'door2',
-      desc:'desc2',
-      status:'close'
-    }]
+     Banners:[],
+     Doors:[],
+     Region:[],
+     Position:''
   },
 
+ 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.setData({
+      Banners:local_banners,
+      Doors:local_doors.data,
+      Region:['上海市','上海市','浦东新区'],
+      Position:'上海市-上海市-浦东新区'
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
-
   /**
    * 生命周期函数--监听页面显示
    */
@@ -66,13 +66,19 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log('onReachBottom')
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    console.log('onShareAppMessage')
+  },
+  bindRegionChange(e){
+    this.setData({
+      Region: e.detail.value,
+      Position:this.data.Region[0]+'-'+this.data.Region[1]+'-'+this.data.Region[2]
+    })
   }
 })
