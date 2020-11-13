1e41be071a13073c11d7413d1fa5fde56f35ef98
Roden <448544937@qq.com>
2020-11-13 22:50:08 +0800

    toolbar

---
+++ Components/SwipperTopBar/index.js
@@ -0,0 +1,23 @@
+// Components/SwipperTopBar/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    datalist:Object
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
