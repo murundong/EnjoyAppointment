e49853c2ed5c0e7328aaa046267b56477b03bc48
Roden <448544937@qq.com>
2020-12-31 00:29:02 +0800

    EditCourse

--- pages/Course/Course.js
+++ pages/Course/Course.js
@@ -1,6 +1,7 @@
 // pages/Course/Course.js
 import urls from '../../utils/urls';
 import request from '../../utils/network.js';
+var util  = require('../../libs/util.js')
 const app = getApp();
 var baseURL= app.globalData.baseMVCURL;
 Page({
@@ -15,7 +16,9 @@ Page({
     baseURL:baseURL,
     _doorId:'',
     _doorName:'',
-    _currendDate:''
+    _currendDate:'',
+    _selectDate:'',
+    _showWeek:''
   },
 
   /**
@@ -29,7 +32,9 @@ Page({
     var day = now.getDate();
     this.setData({
       _doorId:options.doorId,
-      _currendDate: `${year}-${month}-${day}`
+      _currendDate: `${year}-${month}-${day}`,
+      _selectDate: `${year}-${month}-${day}`,
+      _showWeek:`今天-周${util.toWeekDay((new Date()).getDay())}`
     })
   },
 
@@ -39,28 +44,11 @@ Page({
   onReady: function () {
 
   },
-
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    var _that = this;
-    request({
-      url:urls.Courses.GetAdminCourseByDate,
-      data:{
-        door_id:_that.data._doorId,
-        date:_that.data._currendDate
-      },
-      method:'post'
-    }).then(res=>{
-      if(res.errCode==0){
-        _that.setData({
-          _courses:res.data.data,
-          _noData:res.data.total==0
-        })
-        
-      }
-    })
+   this.InitCourseData();
   },
 
   /**
@@ -97,10 +85,58 @@ Page({
   onShareAppMessage: function () {
 
   },
+  InitCourseData(){
+    var _that = this;
+    request({
+      url:urls.Courses.GetAdminCourseByDate,
+      data:{
+        door_id:_that.data._doorId,
+        date:_that.data._selectDate
+      },
+      method:'post'
+    }).then(res=>{
+      if(res.errCode==0){
+        _that.setData({
+          _courses:res.data.data,
+          _noData:res.data.total==0
+        })
+        
+      }
+    })
+  },
   onAddCourse(e){
     var did= e.currentTarget.dataset.doorid;
     wx.navigateTo({
       url: `../CreateCourse/CreateCourse?doorId=${this.data._doorId}`,
     })
+  },
+  bindDayChange(e){
+    this.setData({
+      _selectDate:e.detail.value
+    })
+    if(e.detail.value == this.data._currendDate){
+      this.setData({
+        _showWeek:`今天-周${util.toWeekDay((new Date(e.detail.value)).getDay())}`
+      })
+    }else{
+      this.setData({
+        _showWeek: `周${util.toWeekDay((new Date(e.detail.value)).getDay())}`
+      })
+    }
+    this.InitCourseData();
+  },
+  onSlideEdit(e){
+    var cid = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: `../CreateCourse/CreateCourse?doorId=${this.data._doorId}&courseId=${cid}`,
+    })
+  },
+  onSlideDelete(e){
+    var cid = e.currentTarget.dataset.id;
+    console.log(cid);
+  },
+  onSlideCancel(e){
+    var cid = e.currentTarget.dataset.id;
+    console.log(cid);
   }
 })
